4001343257e884e971ccee85ab98d91c7ed8397b
elepot87 <78643839+elepot87@users.noreply.github.com>
2022-01-23 20:34:55 +0100

    prova di uso di compact

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 class MovieController extends Controller
 {
     public function index() {
-        return view ('movies');
+        // Dati
+        $films = [
+           'Jurassic Park', 'Orgoglio e Pregiudizio', 'IL padrino'
+        ];
+        // View
+        return view ('movies', compact('films'));
     }
 }
--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -4,4 +4,11 @@
 
 @section('main-content')
     <h1 class="title-page">Pagina di archivio dei film </h1>
+
+    <div class="card-film">
+        @foreach ($films as $film)
+            <h3>{{$film}}</h3>
+        @endforeach
+        
+    </div>
 @endsection
